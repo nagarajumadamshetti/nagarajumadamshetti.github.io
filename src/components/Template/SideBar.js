ed78bf617e1b1287072fafda1ecd7d09eb70b83d
nagarajumadamshetti <nagarajumadamshetti@gmail.com>
2023-10-16 19:47:45 -0400

    update profile image

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -9,7 +9,7 @@ const SideBar = () => (
   <section id="sidebar">
     <section id="intro">
       <Link to="/" className="logo">
-        <img src={`${PUBLIC_URL}/images/me.jpg`} alt="" />
+        <img src={`${PUBLIC_URL}/images/profile1.jpg`} alt="" />
       </Link>
       <header>
         <h2>Nagaraj Madamshetti</h2>
